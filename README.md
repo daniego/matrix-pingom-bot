b2480c265107a2dedcef22443701080b234f62d1
Unknown <daniel.floris@gmail.com>
2019-06-17 17:03:02 +0100

    implemented parameters
    more steps
    
    
    simplified functions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # matrix-pingom-bot
+
+This is a bot that receives messages from pingdom and sends it to matrix
+
+# Usage
+Docker environment
+------------------
+Create the .env file based on .env_sample and run the composer
+```
+docker-compose up -d
+```
--- settings.py
+++ settings.py
@@ -2,7 +2,13 @@
 from os import environ
 
 MATRIX_SERVER = environ.get("MATRIX_SERVER", "https://matrix.example.org")
+
 MATRIX_TOKEN = environ.get("MATRIX_TOKEN", "matrix_token")
-MATRIX_ROOM = environ.get("MATRIX_ROOM", "!room_id:example.org")
+
+ACCESS_TOKENS = environ.get('ACCESS_TOKENS', '{ "Identifier name": "thisISaSAMPLEtoken","Identifier name2": "abc",}')
+
+MATRIX_ROOMS =  environ.get("MATRIX_ROOMS", "{ 'test_room': '!room_id:example.org',}")
+
 LOG_LEVEL = environ.get("LOG_LEVEL", "INFO")
+
 debug_enabled = environ.get("debug_enabled", False)
